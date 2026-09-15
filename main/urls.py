3e765f7fc949d7e087e41d65c1d56e6c082e830a
RedStone576 <redgaming576@gmail.com>
2026-09-16 06:23:38 +0700

    feat: form submission views

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_education, show_projects
+from main.views import \
+show_main, \
+show_experience, show_education, show_projects, \
+create_experience, create_education, create_projects
 
 app_name = "main"
 
@@ -9,4 +12,9 @@ urlpatterns = [
     path("experience/", show_experience,  name="show_experience"),
     path("education/",  show_education,   name="show_education"),
     path("projects/",   show_projects,    name="show_projects"),
+    
+    path("experience/create/", create_experience, name="create_experience"),
+    path("education/create/",  create_education,  name="create_education"),
+    path("projects/create/",   create_projects,   name="create_project"),
+    
 ]
--- main/views.py
+++ main/views.py
@@ -1,20 +1,26 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.core import serializers
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 
 from main.models import Experience, Education, Project
+from main.forms import ExperienceForm, EducationForm, ProjectForm
 
-MY_NAME = "Ilham Firmansyah"
+MY_INFO = {
+    "name": "Ilham Firmansyah", 
+    "npm": "2506532643",
+    "study_program": "Sistem Informasi",
+    "study_program_kd": "06.00.12.01",
+    "role": "Insinyur Perangkat Lunak",
+}
+
+###
 
 def show_main(request):
     # mkay context.name are more like title or something bruh
     context = {
-        "name": MY_NAME, 
-        "npm": "2506532643",
-        "study_program": "Sistem Informasi",
-        "study_program_kd": "06.00.12.01",
-        
         "bio": (
             "<ul>"
-            "<li><i>Insinyur Perangkat Lunak</i></li>"
             "<li>Recreational programmer</li>"
             "<li>linguistic enthusiast</li>"
             "<li>supposedly computer philosopher</li>"
@@ -23,40 +29,74 @@ def show_main(request):
             "Loves raw JavaScript at its finest. TypeScript too, if you insist."
         ),
     }
-    return render(request, "index.html", context)
+    
+    return render(request, "index.html", MY_INFO | context)
 
 
 def show_experience(request):
     context = {
-        "name": MY_NAME, 
-        "npm": "2506532643",
-        "study_program": "Sistem Informasi",
-        "study_program_kd": "06.00.12.01",
-        
         "experience_list": Experience.objects.all(),
     }
-    return render(request, "experience.html", context)
+    
+    return render(request, "experience.html", MY_INFO | context)
 
 
 def show_education(request):
     context = {
-        "name": MY_NAME, 
-        "npm": "2506532643",
-        "study_program": "Sistem Informasi",
-        "study_program_kd": "06.00.12.01",
-        
         "education_list": Education.objects.all(),
     }
-    return render(request, "education.html", context)
+    
+    return render(request, "education.html", MY_INFO | context)
 
 
 def show_projects(request):
     context = {
-        "name": MY_NAME, 
-        "npm": "2506532643",
-        "study_program": "Sistem Informasi",
-        "study_program_kd": "06.00.12.01",
-        
         "project_list": Project.objects.all(),
     }
-    return render(request, "projects.html", context)
+    
+    return render(request, "projects.html", MY_INFO | context)
+
+###
+
+def create_experience(request):
+    form = ExperienceForm(request.POST or None)
+ 
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Experience added successfully!")
+        return redirect("main:show_experience")
+ 
+    context = {
+        "form": form,
+    }
+    
+    return render(request, "experience_create.html", MY_INFO | context)
+ 
+ 
+def create_education(request):
+    form = EducationForm(request.POST or None)
+ 
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Education added successfully!")
+        return redirect("main:show_education")
+ 
+    context = {
+        "form": form,
+    }
+    
+    return render(request, "education_create.html", MY_INFO | context)
+
+def create_projects(request):
+    form = ProjectForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Proyek baru berhasil ditambahkan!")
+        return redirect("main:show_projects")
+
+    context = {
+        "form": form,
+    }
+    
+    return render(request, "projects_create.html", MY_INFO | context)

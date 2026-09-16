8b8851861ce95b3f230ca2bbbd2699453608a0a3
RedStone576 <redgaming576@gmail.com>
2026-09-16 07:57:01 +0700

    refactor: combine boilerplate views handler into a generic one because i can

--- main/urls.py
+++ main/urls.py
@@ -1,22 +1,21 @@
 from django.urls import path
 
-from main.views import \
-show_main, \
-show_experience, show_education, show_projects, \
-create_experience, create_education, create_projects, \
-get_projects_json
+from main.views import show_main, show_list, form_view, api_view
 
 app_name = "main"
 
 urlpatterns = [
-    path("",            show_main,        name="show_main"),
-    path("experience/", show_experience,  name="show_experience"),
-    path("education/",  show_education,   name="show_education"),
-    path("projects/",   show_projects,    name="show_projects"),
+    path("", show_main, name="show_main"),
     
-    path("experience/create/", create_experience, name="create_experience"),
-    path("education/create/",  create_education,  name="create_education"),
-    path("projects/create/",   create_projects,   name="create_project"),
-
-    path("api/projects/", get_projects_json, name="get_projects_json"),
+    path("experience/", show_list, {"item_type": "experience"}, name="show_experience"),
+    path("education/",  show_list, {"item_type": "education"},  name="show_education"),
+    path("projects/",   show_list, {"item_type": "projects"},   name="show_projects"),
+    
+    path("experience/create/", form_view, {"item_type": "experience"}, name="create_experience"),
+    path("education/create/",  form_view, {"item_type": "education"},  name="create_education"),
+    path("projects/create/",   form_view, {"item_type": "projects"},   name="create_project"),
+    
+    path("api/experience/", api_view, {"item_type": "experience"}),
+    path("api/education/",  api_view, {"item_type": "education"}),
+    path("api/projects/",   api_view, {"item_type": "projects"}),
 ]
